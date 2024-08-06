602b9acb9cd667e5e50288f08dbb3d2677a08ef7
Damir Feyzulov <feizulov.damir@gmail.com>
2024-08-06 17:21:33 +0300

    rename orders to orderlist

--- src/test/java/yandex/praktikim/parameterized/OrderCreationTest.java
+++ src/test/java/yandex/praktikim/parameterized/OrderCreationTest.java
@@ -1,4 +1,9 @@
 package yandex.praktikim.parameterized;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class OrderCreationTest {
 }
